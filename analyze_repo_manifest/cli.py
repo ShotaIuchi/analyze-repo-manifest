2318fc286073d59a368d4bc784da0e681707bc5f
Shota Iuchi <shotaiuchi.develop@gmail.com>
2025-04-24 22:07:41 +0900

    call test

--- analyze_repo_manifest/cli.py
+++ analyze_repo_manifest/cli.py
@@ -1,2 +1,9 @@
+from analyze_repo_manifest.parse import parse_manifest
+
+
 def main():
-    print("Hello from analyze-repo-manifest!")
+    import os
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    manifest = parse_manifest(os.path.join(current_dir, os.path.pardir, "tests", "data", "manifests", "default.xml"))
+    for remote in manifest.remotes:
+        print(f"Remote name: {remote.name}, fetch: {remote.fetch}")
